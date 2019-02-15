aedb2d149b11141f693065e978827bd89fc6f2b5
rishitoshsingh <rishitoshs@gmail.com>
2019-02-15 17:09:16 +0530

    Fixed

--- app/src/main/java/in/co/bdcoe/tedxakgec/Fragments/AboutUsFragment.kt
+++ app/src/main/java/in/co/bdcoe/tedxakgec/Fragments/AboutUsFragment.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,6 +43,7 @@ class AboutUsFragment : Fragment() {
         about_ted.text = aboutUs.ted
         about_tedx.text = aboutUs.tedx
         about_tedx_akgec.text = aboutUs.tedxAkgec
+        love_text_view.text = Html.fromHtml("&#10084;")
 
         instagram_tedxakgec.setOnClickListener {
             val newIntent = Intent(Intent.ACTION_VIEW,
